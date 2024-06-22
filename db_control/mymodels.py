aa1563a38bd00a3c8edbda7b7cbed36b25e7103f
Ryosuke Inoue <>
2024-06-22 19:42:00 +0900

    デバッグコード削除

--- db_control/mymodels.py
+++ db_control/mymodels.py
@@ -2,14 +2,11 @@ from sqlalchemy import create_engine, Column, Integer, String, Float, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
-from datetime import datetime  # datetimeをインポート
+from datetime import datetime
 
 Base = declarative_base()
-print("Current working directory:", os.getcwd())
 main_path = os.path.dirname(os.path.abspath(__file__))
 path = os.chdir(main_path)
-print(path)
-print("Current working directory:", os.getcwd())
 engine = create_engine('sqlite:///Commerce.db', echo=True)
 Session = sessionmaker(bind=engine)
 
